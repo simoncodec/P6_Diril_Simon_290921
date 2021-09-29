d8359ca3b67f45d871e90ba09226b5fc1aac4573
simon <simondiril@gmail.com>
2021-09-29 12:09:33 +0200

    amelioration du fichier server.js

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
-
+app.use((req, res) => {
+   res.json({ message: 'Votre requête a bien été reçue !' }); 
+});
 
 module.exports = app;
