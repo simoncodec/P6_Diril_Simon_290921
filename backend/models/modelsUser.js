c9268e32e66f2248cb660e0b1c0acd4c550f442b
simon <simondiril@gmail.com>
2021-09-30 23:14:13 +0200

    mise en place du plugin unique validator mongoose

---
+++ backend/models/modelsUser.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+// constante qui permet de vérifier l'existence ou non de cette clé dans la BDD
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchemas = mongoose.Schema ({
+email: {type: String, required: true, unique: true},
+password: {type: String, required: true}
+});
+
+// on applique le plugin pour vérifier la valeur unique de la donnée
+userSchemas.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchemas);
